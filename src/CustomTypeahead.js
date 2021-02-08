5c025b7e680c0bb6a6a5a5fa39b87144021cc865
restee <resty.artiaga@gmail.com>
2021-02-09 00:46:40 +0800

    Added handling of item click

--- src/CustomTypeahead.js
+++ src/CustomTypeahead.js
@@ -9,11 +9,18 @@ function CustomTypeahead({ fontSize, dropdownHeight }) {
     const inputRef = useRef(null);
     const [inputValue, setInputValue] = useState('');
     const [inputWidth, setInputWidth] = useState(0);
+    const [showDialog, setShowDialog] = useState(false);
 
     const onTextChangeHandler = event => {
         setInputValue(event.target.value);
         console.log(event.target.value);
+        setShowDialog(true);
     };
+
+    const onItemSelected = item => {
+        setInputValue(item.login);
+        setShowDialog(false);
+    }
 
     useEffect(() => {
         if (inputRef.current) {
@@ -27,7 +34,7 @@ function CustomTypeahead({ fontSize, dropdownHeight }) {
         <div>
             <form>
                 <input ref={inputRef} type="text" style={{ width: '100%', padding: '5px', fontSize: '20px' }} name="name" value={inputValue} onChange={onTextChangeHandler} />
-                <DropdownResults user={inputValue} width={inputWidth} />
+                <DropdownResults user={inputValue} width={inputWidth} onItemSelected={onItemSelected} show={showDialog}/>
             </form>
         </div>
     );
--- src/DropDownComponent.js
+++ src/DropDownComponent.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import useSWR from "swr";
+import './App.css';
 
 const fetcher = url => fetch(url).then(res => res.json());
 
 
-function DropdownResults({ height, user, width }) {
+function DropdownResults({ height, user, width, onItemSelected, show}) {
 
     const { data, error } = useSWR(
         user ? `https://api.github.com/search/users?q=${user}` : null,
         fetcher
-    );    
+    );
+
+    const onItemClicked = value => {
+        console.log("Item clicked = ", value);
+        onItemSelected(value);
+    };
+
 
     if (!height)
         height = '30vh';
 
-    if (!user)
+    if (!user || error || !show)
         return null;
 
     const itemViews = [];
@@ -23,9 +30,11 @@ function DropdownResults({ height, user, width }) {
         for (const [index, value] of data.items.entries()) {
             itemViews.push(
                 <div key={index + index - 1}>
-                    <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', color: 'black', marginLeft: "5%", marginRight: "5%", padding: '2px' }}>
-                        <img src={value.avatar_url} height="60%" width="20%" />
-                        <p style={{ marginLeft: "10%" }}>{value.login}</p>
+                    <div
+                        className="DropDownItem"
+                        onClick={() => { onItemClicked(value) }}>
+                        <img src={value.avatar_url} height="60%" width="20%" style={{ marginLeft: '5%' }} />
+                        <p style={{ marginLeft: "10%", marginRight: '5%' }}>{value.login}</p>
                     </div>
                     {index < data.items.length - 1 && <div style={{ backgroundColor: 'gray', width: '90%', marginLeft: '5%', marginTop: '1%', marginBottom: '1%', height: '1px' }} />}
                 </div>)
