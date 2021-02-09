cb14d658f4bf943dfd2a3ef59e4a77273436db1e
restee <resty.artiaga@gmail.com>
2021-02-09 23:27:30 +0800

    Improved fluidity css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="col2" style={{ marginTop: "50px" }}>
+        <div className="col-sm4 col-md3 col-l2" style={{ marginTop: "50px" }}>
           <CustomTypeahead dropdownHeight="40vh" style={{fontSize:'20px'}}/>
         </div>
         <div>Input Username</div>
